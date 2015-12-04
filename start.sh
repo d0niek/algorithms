4e5050d8948193a96b764cd7ee3a20bb0cd51ae4
d0niek <glinka18@wp.pl>
2015-12-04 21:43:32 +0100

    Deleted unnecessary apostrophes

--- start.sh
+++ start.sh
@@ -2,13 +2,13 @@
 path=$1
 name=$2
 
-if [ ! -d "$path/$name/tests" ]; then
-    `mkdir -p $path/$name/tests`;
+if [ ! -d $path/$name/tests ]; then
+    mkdir -p $path/$name/tests;
 fi
 
-`touch $path/$name/tests/test0.in`;
-`touch $path/$name/tests/test0.out`;
+touch $path/$name/tests/test0.in;
+touch $path/$name/tests/test0.out;
 
-if [ ! -f "$path/$name/$name.cpp" ]; then
-    `cp ./template.cpp $path/$name/$name.cpp`;
+if [ ! -f $path/$name/$name.cpp ]; then
+    cp ./template.cpp $path/$name/$name.cppx;
 fi
