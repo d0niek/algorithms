3b26be4811519807ea8680b0c8671aa8a24b21b1
d0niek <glinka18@wp.pl>
2016-03-16 17:32:48 +0100

    Run tests base on name

--- test.sh
+++ test.sh
@@ -1,14 +1,19 @@
 #/bin/bash
 path=$1
 name=$2
-number=$3
 out=$path/$name/out
 
 if [ ! -d $out ]; then
     mkdir $out;
 fi
 
-for i in `seq 0 $number`; do
-    /usr/bin/time -f "test $i: %e" $path/$name/$name < $path/$name/tests/test$i.in > $out/test$i.out;
-    diff -wqB $path/$name/tests/test$i.out $out/test$i.out;
+i=1
+
+for file in $path/$name/tests/*.in; do
+    file=$(echo $file | cut -d '/' -f 5 | cut -d '.' -f 1)
+
+    /usr/bin/time -f "test $i: %e" $path/$name/$name < $path/$name/tests/$file.in > $out/$file.out;
+    diff -wqB $path/$name/tests/$file.out $out/$file.out;
+
+    ((i++))
 done;
